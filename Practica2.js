067135a968fb2e810c41e26d98a39fe8c2793886
Efrain7864 <91640720+Efrain7864@users.noreply.github.com>
2022-12-06 21:21:54 -0600

    Add files via upload

---
+++ Practica2.js
@@ -0,0 +1,8 @@
+let frase1="El sabia que ella no la queria, pero aun sabiendolo no dejaba de emocionarse cada vez que la miraba"
+let frase2=frase1.split(",");
+let frase3=frase1.split(" ");
+
+console.log("Frase 1: ", frase1)
+console.log("Frase 2: ", frase2)
+console.log("Frase 3: ", frase3)
+
