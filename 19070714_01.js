b9b932ba0bfbc2c8d8bed8a1645c0baf6109ec58
Efrain7864 <91640720+Efrain7864@users.noreply.github.com>
2022-12-06 21:11:59 -0600

    Add files via upload

---
+++ 19070714_01.js
@@ -0,0 +1,6 @@
+let name= '          Camarillo Contreras Oscar Efrain               ';
+console.log('Sin trim: ', name);
+console.log('Con trimEnd: ', name.trimEnd());
+console.log('Con trimStart: ', name.trimStart());
+
+//Lenguajes y Automatas II
